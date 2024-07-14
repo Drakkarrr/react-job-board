ee598f4b9c04ea3b017299565e65877da066f609
Drakkarrr <drakkar.alpha00@gmail.com>
2024-07-14 13:33:16 -0700

    trim job description state logic show added

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const Job = ({ job }) => {
+  const [showFullDescription, setShowFullDescription] = React.useState(false);
+
+  let jobDescription = job.description;
+
+  if (!showFullDescription) {
+    jobDescription = jobDescription.substring(0, 90) + '...';
+  }
+
   return (
     <div key={job.id} className='relative bg-white shadow-md rounded-xl'>
       <div className='p-4'>
@@ -9,7 +17,10 @@ const Job = ({ job }) => {
           <h3 className='text-xl font-bold'>{job.title}</h3>
         </div>
 
-        <div className='mb-5'>{job.description}</div>
+        <div className='mb-5'>{jobDescription}</div>
+        <button className='mb-5 text-indigo-500 hover:font-semibold'>
+          Show More
+        </button>
 
         <h3 className='mb-2 text-indigo-500'>{job.salary} / Year</h3>
 
