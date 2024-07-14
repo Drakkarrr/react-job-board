bac5708982da607fdb61312dd03cb0190c7a2596
Drakkarrr <drakkar.alpha00@gmail.com>
2024-07-14 11:26:55 -0700

    module alias configured, separate navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,13 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <>
-      <nav className='bg-indigo-700 border-b border-indigo-500'>
-        <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-          <div className='flex h-20 items-center justify-between'>
-            <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-              <a
-                className='flex flex-shrink-0 items-center mr-4'
-                href='/index.html'>
-                {/* <img
-                  className='h-10 w-auto'
-                  src='images/logo.png'
-                  alt='React Jobs'
-                /> */}
-                <h1 className='hidden md:block text-white text-2xl font-bold ml-2'>
-                  ReactJobBoard
-                </h1>
-              </a>
-              <div className='md:ml-auto'>
-                <div className='flex space-x-2'>
-                  <a
-                    href='/index.html'
-                    className='text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
-                    Home
-                  </a>
-                  <a
-                    href='/jobs.html'
-                    className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
-                    Jobs
-                  </a>
-                  <a
-                    href='/add-job.html'
-                    className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
-                    Add Job
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
 
-      <section className='bg-indigo-700 py-20 mb-4'>
-        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center'>
+      <section className='py-20 mb-4 bg-indigo-700'>
+        <div className='flex flex-col items-center px-4 mx-auto max-w-7xl sm:px-6 lg:px-8'>
           <div className='text-center'>
             <h1 className='text-4xl font-extrabold text-white sm:text-5xl md:text-6xl'>
               Become a React Dev
@@ -57,27 +20,27 @@ const App = () => {
       </section>
 
       <section className='py-4'>
-        <div className='container-xl lg:container m-auto'>
-          <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
-            <div className='bg-gray-100 p-6 rounded-lg shadow-md'>
+        <div className='m-auto container-xl lg:container'>
+          <div className='grid grid-cols-1 gap-4 p-4 rounded-lg md:grid-cols-2'>
+            <div className='p-6 bg-gray-100 rounded-lg shadow-md'>
               <h2 className='text-2xl font-bold'>For Developers</h2>
               <p className='mt-2 mb-4'>
                 Browse our React jobs and start your career today
               </p>
               <a
                 href='/jobs.html'
-                className='inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700'>
+                className='inline-block px-4 py-2 text-white bg-black rounded-lg hover:bg-gray-700'>
                 Browse Jobs
               </a>
             </div>
-            <div className='bg-indigo-100 p-6 rounded-lg shadow-md'>
+            <div className='p-6 bg-indigo-100 rounded-lg shadow-md'>
               <h2 className='text-2xl font-bold'>For Employers</h2>
               <p className='mt-2 mb-4'>
                 List your job to find the perfect developer for the role
               </p>
               <a
                 href='/add-job.html'
-                className='inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600'>
+                className='inline-block px-4 py-2 text-white bg-indigo-500 rounded-lg hover:bg-indigo-600'>
                 Add Job
               </a>
             </div>
@@ -85,16 +48,16 @@ const App = () => {
         </div>
       </section>
 
-      <section className='bg-blue-50 px-4 py-10'>
-        <div className='container-xl lg:container m-auto'>
-          <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
+      <section className='px-4 py-10 bg-blue-50'>
+        <div className='m-auto container-xl lg:container'>
+          <h2 className='mb-6 text-3xl font-bold text-center text-indigo-500'>
             Browse Jobs
           </h2>
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            <div className='bg-white rounded-xl shadow-md relative'>
+          <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
+            <div className='relative bg-white shadow-md rounded-xl'>
               <div className='p-4'>
                 <div className='mb-6'>
-                  <div className='text-gray-600 my-2'>Full-Time</div>
+                  <div className='my-2 text-gray-600'>Full-Time</div>
                   <h3 className='text-xl font-bold'>Senior React Developer</h3>
                 </div>
 
@@ -104,13 +67,13 @@ const App = () => {
                   HTML, CSS, and JavaScript...
                 </div>
 
-                <h3 className='text-indigo-500 mb-2'>$70 - $80K / Year</h3>
+                <h3 className='mb-2 text-indigo-500'>$70 - $80K / Year</h3>
 
-                <div className='border border-gray-100 mb-5'></div>
+                <div className='mb-5 border border-gray-100'></div>
 
-                <div className='flex flex-col lg:flex-row justify-between mb-4'>
-                  <div className='text-orange-700 mb-3'>
-                    <i className='fa-solid fa-location-dot text-lg'></i>
+                <div className='flex flex-col justify-between mb-4 lg:flex-row'>
+                  <div className='mb-3 text-orange-700'>
+                    <i className='text-lg fa-solid fa-location-dot'></i>
                     Boston, MA
                   </div>
                   <a
@@ -122,10 +85,10 @@ const App = () => {
               </div>
             </div>
 
-            <div className='bg-white rounded-xl shadow-md relative'>
+            <div className='relative bg-white shadow-md rounded-xl'>
               <div className='p-4'>
                 <div className='mb-6'>
-                  <div className='text-gray-600 my-2'>Remote</div>
+                  <div className='my-2 text-gray-600'>Remote</div>
                   <h3 className='text-xl font-bold'>
                     Front-End Engineer (React)
                   </h3>
@@ -136,13 +99,13 @@ const App = () => {
                   are looking for a motivated individual with a passion...
                 </div>
 
-                <h3 className='text-indigo-500 mb-2'>$70K - $80K / Year</h3>
+                <h3 className='mb-2 text-indigo-500'>$70K - $80K / Year</h3>
 
-                <div className='border border-gray-100 mb-5'></div>
+                <div className='mb-5 border border-gray-100'></div>
 
-                <div className='flex flex-col lg:flex-row justify-between mb-4'>
-                  <div className='text-orange-700 mb-3'>
-                    <i className='fa-solid fa-location-dot text-lg'></i>
+                <div className='flex flex-col justify-between mb-4 lg:flex-row'>
+                  <div className='mb-3 text-orange-700'>
+                    <i className='text-lg fa-solid fa-location-dot'></i>
                     Miami, FL
                   </div>
                   <a
@@ -154,10 +117,10 @@ const App = () => {
               </div>
             </div>
 
-            <div className='bg-white rounded-xl shadow-md relative'>
+            <div className='relative bg-white shadow-md rounded-xl'>
               <div className='p-4'>
                 <div className='mb-6'>
-                  <div className='text-gray-600 my-2'>Remote</div>
+                  <div className='my-2 text-gray-600'>Remote</div>
                   <h3 className='text-xl font-bold'>React.js Developer</h3>
                 </div>
 
@@ -167,13 +130,13 @@ const App = () => {
                   make a difference...
                 </div>
 
-                <h3 className='text-indigo-500 mb-2'>$70K - $80K / Year</h3>
+                <h3 className='mb-2 text-indigo-500'>$70K - $80K / Year</h3>
 
-                <div className='border border-gray-100 mb-5'></div>
+                <div className='mb-5 border border-gray-100'></div>
 
-                <div className='flex flex-col lg:flex-row justify-between mb-4'>
-                  <div className='text-orange-700 mb-3'>
-                    <i className='fa-solid fa-location-dot text-lg'></i>
+                <div className='flex flex-col justify-between mb-4 lg:flex-row'>
+                  <div className='mb-3 text-orange-700'>
+                    <i className='text-lg fa-solid fa-location-dot'></i>
                     Brooklyn, NY
                   </div>
                   <a
@@ -188,10 +151,10 @@ const App = () => {
         </div>
       </section>
 
-      <section className='m-auto max-w-lg my-10 px-6'>
+      <section className='max-w-lg px-6 m-auto my-10'>
         <a
           href='jobs.html'
-          className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'>
+          className='block px-6 py-4 text-center text-white bg-black rounded-xl hover:bg-gray-700'>
           View All Jobs
         </a>
       </section>
