9b65246068404ad14691ab30baf92622e0cf3d3a
Drakkarrr <drakkar.alpha00@gmail.com>
2024-07-24 07:12:51 -0700

    fix: navigation issue, on rerender and routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const navlinks = [
-    { title: 'Home', url: '/index.html' },
-    { title: 'Jobs', url: '/jobs.html' },
-    { title: 'Add Job', url: '/add-job.html' },
+    { title: 'Home', url: '/' },
+    { title: 'Jobs', url: '/jobs' },
+    { title: 'Add Job', url: '/add-job' },
   ];
 
   const activeLink = navlinks.url === window.location.pathname;
@@ -14,9 +15,7 @@ const Navbar = () => {
       <div className='px-2 mx-auto max-w-7xl sm:px-6 lg:px-8'>
         <div className='flex items-center justify-between h-20'>
           <div className='flex items-center justify-center flex-1 md:items-stretch md:justify-start'>
-            <a
-              className='flex items-center flex-shrink-0 mr-4'
-              href='/index.html'>
+            <a className='flex items-center flex-shrink-0 mr-4' href='/'>
               {/* <img
                   className='w-auto h-10'
                   src='images/logo.png'
@@ -29,12 +28,12 @@ const Navbar = () => {
             <div className='md:ml-auto'>
               <div className='flex space-x-2'>
                 {navlinks.map((link) => (
-                  <a
+                  <Link
                     key={link.title}
-                    href={link.url}
+                    to={link.url}
                     className='px-3 py-2 text-[18px] font-medium text-white rounded-md hover:bg-indigo-500'>
                     {link.title}
-                  </a>
+                  </Link>
                 ))}
               </div>
             </div>
