235f3da43e304a7f4c067c1f4bb6bc776b38d353
Drakkarrr <drakkar.alpha00@gmail.com>
2024-07-24 13:11:54 -0700

    render title dynamically, slice jobs reuse home to jobs page list

--- src/components/Joblisting.jsx
+++ src/components/Joblisting.jsx
@@ -10,7 +10,7 @@ const Joblisting = ({ isHome = false }) => {
     <section className='px-4 py-10 bg-blue-50'>
       <div className='m-auto container-xl lg:container'>
         <h2 className='mb-6 text-3xl font-bold text-center text-indigo-500'>
-          Browse Jobs
+          {isHome ? 'Featured Jobs' : 'Browse All Jobs'}
         </h2>
         <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
           {jobListing.map((job) => (
