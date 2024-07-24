88315cb3b8a11e764b221c0d81c74db93e9ed510
Drakkarrr <drakkar.alpha00@gmail.com>
2024-07-24 12:01:49 -0700

    Active link styling complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const navlinks = [
@@ -8,14 +8,17 @@ const Navbar = () => {
     { title: 'Add Job', url: '/add-job' },
   ];
 
-  const activeLink = navlinks.url === window.location.pathname;
+  const activeLink = ({ isActive }) =>
+    isActive
+      ? 'px-3 py-2 text-[18px] font-medium text-white rounded-md bg-black'
+      : 'px-3 py-2 text-[18px] font-medium text-white rounded-md hover:bg-indigo-500';
 
   return (
     <nav className='bg-indigo-700 border-b border-indigo-500'>
       <div className='px-2 mx-auto max-w-7xl sm:px-6 lg:px-8'>
         <div className='flex items-center justify-between h-20'>
           <div className='flex items-center justify-center flex-1 md:items-stretch md:justify-start'>
-            <Link className='flex items-center flex-shrink-0 mr-4' to='/'>
+            <NavLink className='flex items-center flex-shrink-0 mr-4' to='/'>
               {/* <img
                   className='w-auto h-10'
                   src='images/logo.png'
@@ -24,16 +27,16 @@ const Navbar = () => {
               <h1 className='hidden ml-2 text-2xl font-bold text-white md:block'>
                 ReactJobBoard
               </h1>
-            </Link>
+            </NavLink>
             <div className='md:ml-auto'>
               <div className='flex space-x-2'>
                 {navlinks.map((link) => (
-                  <Link
+                  <NavLink
                     key={link.title}
                     to={link.url}
-                    className='px-3 py-2 text-[18px] font-medium text-white rounded-md hover:bg-indigo-500'>
+                    className={activeLink}>
                     {link.title}
-                  </Link>
+                  </NavLink>
                 ))}
               </div>
             </div>
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -10,7 +10,7 @@ const NotFoundPage = () => {
       <Link
         to='/'
         className='px-3 py-2 mt-4 text-white bg-indigo-700 rounded-md hover:bg-indigo-900'>
-        Go Back
+        Back to Home
       </Link>
     </section>
   );
