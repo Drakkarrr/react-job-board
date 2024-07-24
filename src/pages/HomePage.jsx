7500b12907e502e3713ffe043da1035f476de2be
Drakkarrr <drakkar.alpha00@gmail.com>
2024-07-24 07:04:20 -0700

    fet: completed dynamic rouiting and layout implementation fix: location icon issue not showing up

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Hero from '@/components/Hero';
+import HomeCards from '@/components/HomeCards';
+import Joblisting from '@/components/Joblisting';
+import ViewAllJobs from '@/components/ViewAllJobs';
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <HomeCards />
+      <Joblisting />
+      <ViewAllJobs />
     </>
   );
 };
