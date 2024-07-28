c31c4d14d31b1bd4de1bc4fd3c367511e713c874
Drakkarrr <drakkar.alpha00@gmail.com>
2024-07-28 04:33:52 -0700

    fix: dataloader error on fetching data params fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage loader={jobLoader} />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
       <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,28 +1,8 @@
-import { useState, useEffect } from 'react';
 import { useParams, useLoaderData } from 'react-router-dom';
-import Loader from '@/components/Loader';
 
 const JobPage = () => {
-  // const [job, setJob] = useState([]);
-  // const [loading, setLoading] = useState(false);
   const { id } = useParams();
   const job = useLoaderData();
-
-  // useEffect(() => {
-  //   const fetchJob = async () => {
-  //     try {
-  //       const res = await fetch(`/api/jobs/${id}`);
-  //       const data = await res.json();
-  //       setJob(data);
-  //     } catch (error) {
-  //       console.log(`Error fetching data: ${error}`);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchJob();
-  // }, []);
 
   return (
     <h1>
@@ -31,8 +11,8 @@ const JobPage = () => {
   );
 };
 
-const jobLoader = async (id) => {
-  const res = await fetch(`/api/jobs/${id}`);
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
   const data = await res.json();
   return data;
 };
